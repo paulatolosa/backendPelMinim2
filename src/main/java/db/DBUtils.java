6a88d122af4d66c41a003706b28158c79ff6f176
paulatolosallop <paula.tolosa@estudiantat.upc.edu>
2025-12-09 20:41:05 +0100

    estil items i imatges modificades, problema amb la imatge health_potion.png, despres miro que falla

--- src/main/java/db/DBUtils.java
+++ src/main/java/db/DBUtils.java
@@ -9,7 +9,7 @@ public class DBUtils {
     public static final String DB_NAME = "db_backend";
     public static final String DB_HOST = "127.0.0.1";
     public static final String DB_USER = "root";
-    public static final String DB_PASS = "root";
+    public static final String DB_PASS = "manhartmh800";
     public static final String DB_PORT = "3306";
 
     public static String getDb() {
